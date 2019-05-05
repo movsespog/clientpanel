4e56462c4190a1f700c3b8bf57e46b8622c3f929
movsespog <poghosyan_movses@yahoo.com>
2019-05-05 21:02:25 +0400

    Added Client and Dashboard components

--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Clients from '../clients/Clients';
+import Sidebar from '../layout/Sidebar';
 
 export default () => {
     return(
-        <div>
-            <h1>Dashboard</h1>
+        <div className="row">
+            <div className="col-md-10">
+                <Clients />
+            </div>
+            <div className="col-md-2">
+                <Sidebar/>
+            </div>
         </div>
     );
 }
