c3245c5ce4ea7043954069b873bbb79356fdfc17
movsespog <poghosyan_movses@yahoo.com>
2019-05-08 18:07:23 +0400

    Compelted project

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -2,12 +2,13 @@ import React from 'react';
 import spinner from './spinner.gif';
 
 export default () => {
-    return(
-      <div>
-        <img
-            src={spinner}
-            alt="Loading..."
-            style={{ width: '200px', margin: 'auto', display: 'block' }}/>
-      </div>
-    );
-}
+  return (
+    <div>
+      <img
+        src={spinner}
+        alt="Loading..."
+        style={{ width: '200px', margin: 'auto', display: 'block' }}
+      />
+    </div>
+  );
+};
